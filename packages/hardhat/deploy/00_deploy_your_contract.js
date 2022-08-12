a247b8d4151a4b6feb55e43aeb74ca33f8ed3140
Jaxcoder <codenamejason@outlook.com>
2022-08-11 21:18:34 -0400

    Initialize dependabot/npm_and_yarn/protobufjs-6.11.3

--- packages/hardhat/deploy/00_deploy_your_contract.js
+++ packages/hardhat/deploy/00_deploy_your_contract.js
@@ -29,14 +29,11 @@ module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
   const YourContract = await ethers.getContract("YourContract", deployer);
   /*  await YourContract.setPurpose("Hello");
   
-    // To take ownership of yourContract using the ownable library uncomment next line and add the 
-    // address you want to be the owner. 
-    
-    await YourContract.transferOwnership(
-      "ADDRESS_HERE"
-    );
+    To take ownership of yourContract using the ownable library uncomment next line and add the 
+    address you want to be the owner. 
+    // await yourContract.transferOwnership(YOUR_ADDRESS_HERE);
 
-    //const YourContract = await ethers.getContractAt('YourContract', "0xaAC799eC2d00C013f1F11c37E654e59B0429DF6A") //<-- if you want to instantiate a version of a contract at a specific address!
+    //const yourContract = await ethers.getContractAt('YourContract', "0xaAC799eC2d00C013f1F11c37E654e59B0429DF6A") //<-- if you want to instantiate a version of a contract at a specific address!
   */
 
   /*
