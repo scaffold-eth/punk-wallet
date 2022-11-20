30b89950274922187ac58715a5d1752c5c6bd53b
Austin Griffith <austin@ethereum.org>
2022-11-20 09:56:05 -0700

    add analytics and rpc2

--- packages/react-app/src/constants.js
+++ packages/react-app/src/constants.js
@@ -1,5 +1,5 @@
 // MY INFURA_ID, SWAP IN YOURS FROM https://infura.io/dashboard/ethereum
-export const INFURA_ID = "95ff362df12840c98fa418bcd4b27c8a";
+export const INFURA_ID = "5b3aa68d82264f59bb6a1874cb3c23ea"; // trying the  emoji.support key
 
 // MY ETHERSCAN_ID, SWAP IN YOURS FROM https://etherscan.io/myapikey
 export const ETHERSCAN_KEY = "PSW8C433Q667DVEX5BCRMGNAH9FSGFZ7Q8";
@@ -302,7 +302,7 @@ export const NETWORKS = {
     color: "#ceb0fa",
     chainId: 1,
     price: "uniswap",
-    rpcUrl: `https://mainnet.infura.io/v3/${INFURA_ID}`,
+    rpcUrl: `https://rpc2.scaffoldeth.io:48544`,
     blockExplorer: "https://etherscan.io/",
   },
   optimism: {
