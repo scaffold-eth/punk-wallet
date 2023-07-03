9ea4b357ef138c27712fbf3f3b67b3e3b7593d23
Tamas Molnar <tamas.molnar@liferay.com>
2023-07-03 18:16:08 +0200

    ERC20 tokens (#40)
    * Arbitrum: ARB, USDC, USDT and DAI tokens
    
    * Optimism: OP, USDC, USDT and DAI tokens
    
    * Mainnet: USDC, USDT and DAI tokens
    
    ---------
    
    Co-authored-by: moltam89 <moltam89@gmail.com>

--- packages/react-app/src/constants.js
+++ packages/react-app/src/constants.js
@@ -304,6 +304,30 @@ export const NETWORKS = {
     price: "uniswap",
     rpcUrl: `https://rpc2.scaffoldeth.io:48544`,
     blockExplorer: "https://etherscan.io/",
+    erc20Tokens: [
+      {
+        name: "USDC",
+        address: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
+        decimals: 6,
+        imgSrc: "/USDC.png"
+      },
+      {
+        name: "USDT",
+        address: "0xdAC17F958D2ee523a2206206994597C13D831ec7",
+        decimals: 6,
+        imgSrc: "/USDT.png"
+      },
+      {
+        name: "DAI",
+        address: "0x6B175474E89094C44Da98b954EedeAC495271d0F",
+        decimals: 18,
+        imgSrc: "/DAI.png"
+      },
+    ],
+    nativeToken: {
+      name:"ETH",
+      imgSrc:"/ETH.png"
+    }
   },
   buidlguidl: {
     name: "buidlguidl",
@@ -330,6 +354,36 @@ export const NETWORKS = {
     rpcUrl: `https://opt-mainnet.g.alchemy.com/v2/gzr_xuzv2SPwbPchC9Z41qmfodlDglKp`,
     //rpcUrl: `https://mainnet.optimism.io`,
     //gasPrice: 1000000,
+    erc20Tokens: [
+      {
+        name: "OP",
+        address: "0x4200000000000000000000000000000000000042",
+        decimals: 18,
+        imgSrc: "/OP.png"
+      },
+      {
+        name: "USDC",
+        address: "0x7F5c764cBc14f9669B88837ca1490cCa17c31607",
+        decimals: 6,
+        imgSrc: "/USDC.png"
+      },
+      {
+        name: "USDT",
+        address: "0x94b008aA00579c1307B0EF2c499aD98a8ce58e58",
+        decimals: 6,
+        imgSrc: "/USDT.png"
+      },
+      {
+        name: "DAI",
+        address: "0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1",
+        decimals: 18,
+        imgSrc: "/DAI.png"
+      },
+    ],
+    nativeToken: {
+      name:"ETH",
+      imgSrc:"/ETH.png"
+    }
   },
   arbitrum: {
     name: "arbitrum",
@@ -339,6 +393,36 @@ export const NETWORKS = {
     blockExplorer: "https://arbiscan.io/",
     rpcUrl: `https://arb-mainnet.g.alchemy.com/v2/tYM-Tr8c9dHV5a8AgvXnVmS9e-xvoxeM`,
     //gasPrice: 1000000000,// TODO ASK RPC
+    erc20Tokens: [
+      {
+        name: "ARB",
+        address: "0x912CE59144191C1204E64559FE8253a0e49E6548",
+        decimals: 18,
+        imgSrc: "/ARB.png"
+      },
+      {
+        name: "USDC",
+        address: "0xaf88d065e77c8cC2239327C5EDb3A432268e5831",
+        decimals: 6,
+        imgSrc: "/USDC.png"
+      },
+      {
+        name: "USDT",
+        address: "0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9",
+        decimals: 6,
+        imgSrc: "/USDT.png"
+      },
+      {
+        name: "DAI",
+        address: "0xda10009cbd5d07dd0cecc66161fc93d7c9000da1",
+        decimals: 18,
+        imgSrc: "/DAI.png"
+      },
+    ],
+    nativeToken: {
+      name:"ETH",
+      imgSrc:"/ETH.png"
+    }
   },
   gnosis: {
     name: "gnosis",
