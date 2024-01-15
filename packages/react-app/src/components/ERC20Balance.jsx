771d04723cf7e4f746fa89fa7aa55b1adba463ed
moltam89 <moltam89@gmail.com>
2024-01-15 20:36:16 +0100

    Prettier - ERC20Balance.jsx

--- packages/react-app/src/components/ERC20Balance.jsx
+++ packages/react-app/src/components/ERC20Balance.jsx
@@ -4,40 +4,35 @@ import { Spin } from "antd";
 
 import { getTokenBalance } from "../helpers/ERC20Helper";
 
-export default function ERC20Balance({token, rpcURL, size, address}) {
-    const [balance, setBalance] = useState(null);
-    const [loading, setLoading] = useState(true);
+export default function ERC20Balance({ token, rpcURL, size, address }) {
+  const [balance, setBalance] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        async function getBalance() {
-            if (!address) {
-                return;
-            }
+  useEffect(() => {
+    async function getBalance() {
+      if (!address) {
+        return;
+      }
 
-            setLoading(true);
+      setLoading(true);
 
-            try {
-                setBalance(await getTokenBalance(token, rpcURL, address));
-            }
-            catch (error) {
-                console.error("Coudn't fetch balance", error)
-            }
+      try {
+        setBalance(await getTokenBalance(token, rpcURL, address));
+      } catch (error) {
+        console.error("Coudn't fetch balance", error);
+      }
 
-            setLoading(false);
-        }
+      setLoading(false);
+    }
 
-        getBalance();
-    }, [address, token, rpcURL])
+    getBalance();
+  }, [address, token, rpcURL]);
 
-    return (
-        <div>
-            <span style={{ verticalAlign: "middle", fontSize: size ? size : 24, padding: 8,}} >
-                {loading ? 
-                    <Spin/>
-                    : 
-                    balance && balance
-                }
-            </span>
-        </div>
-    );
+  return (
+    <div>
+      <span style={{ verticalAlign: "middle", fontSize: size ? size : 24, padding: 8 }}>
+        {loading ? <Spin /> : balance && balance}
+      </span>
+    </div>
+  );
 }
