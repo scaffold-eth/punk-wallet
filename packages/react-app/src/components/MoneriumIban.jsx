26d5ad361e580387872e2d505c48b2edd929a592
moltam89 <moltam89@gmail.com>
2024-01-16 10:23:08 +0100

    Prettier - MoneriumIban.jsx

--- packages/react-app/src/components/MoneriumIban.jsx
+++ packages/react-app/src/components/MoneriumIban.jsx
@@ -1,232 +1,201 @@
 import React, { useState } from "react";
 
-import QR from 'qrcode.react';
+import QR from "qrcode.react";
 
 import { Alert, Input, message } from "antd";
 import { CopyOutlined, QrcodeOutlined, WarningOutlined } from "@ant-design/icons";
 
 import { LogoOnLogo, TokenDisplay } from "./";
 import { getShortAddress } from "../helpers/MoneriumHelper";
-import { useLocalStorage} from "../hooks";
+import { useLocalStorage } from "../hooks";
 
 //const generateQrCode = require('sepa-payment-qr-code')
 import { generateQrCode } from "../helpers/SepaPaymentQrCodeHelper";
 
-const handleCopy = async (iban) => {
-    let copySuccessful = false;
+const handleCopy = async iban => {
+  let copySuccessful = false;
 
-    try {
-        await navigator.clipboard.writeText(iban);
-        copySuccessful = true;
-    }
-    catch (error) {
-        console.error("navigator.clipboard.writeText is not supported by your browser", error);
-    }
-
-    if (!copySuccessful) {
-        const el = document.createElement('textarea');
-        el.value = iban;
-        document.body.appendChild(el);
-        el.select();
-        document.execCommand('copy');
-        document.body.removeChild(el);
-    }
-
-    message.success(
-        <span style={{position:"relative"}}>
-            Copied IBAN
-        </span>
-    );
+  try {
+    await navigator.clipboard.writeText(iban);
+    copySuccessful = true;
+  } catch (error) {
+    console.error("navigator.clipboard.writeText is not supported by your browser", error);
   }
 
-export default function MoneriumIban( { clientData, currentPunkAddress } ) {
-    const accountArrayIban = clientData.accountArrayIban;
-    const ibanObject = accountArrayIban[0]; // ToDo: Handle multiple ibans
-    const ibanAccountAddress = ibanObject?.address;
+  if (!copySuccessful) {
+    const el = document.createElement("textarea");
+    el.value = iban;
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand("copy");
+    document.body.removeChild(el);
+  }
 
-    const name = clientData.name;
-    const iban = ibanObject?.iban;
+  message.success(<span style={{ position: "relative" }}>Copied IBAN</span>);
+};
 
-    let ibanAccountAddressIsDifferent = false;
+export default function MoneriumIban({ clientData, currentPunkAddress }) {
+  const accountArrayIban = clientData.accountArrayIban;
+  const ibanObject = accountArrayIban[0]; // ToDo: Handle multiple ibans
+  const ibanAccountAddress = ibanObject?.address;
 
-    if (currentPunkAddress.toLowerCase() != ibanAccountAddress?.toLowerCase()) {
-        ibanAccountAddressIsDifferent = true;
-    }
+  const name = clientData.name;
+  const iban = ibanObject?.iban;
 
-    const [amount, setAmount] = useState("");
-    const [memo, setMemo] = useState("");
+  let ibanAccountAddressIsDifferent = false;
 
-    const [showIbanQR, setShowIbanQR] = useLocalStorage("showIbanQR", true); 
-    const [showWarningMessage, setShowWarningMessage] = useLocalStorage("showIbanWarning", true); 
+  if (currentPunkAddress.toLowerCase() != ibanAccountAddress?.toLowerCase()) {
+    ibanAccountAddressIsDifferent = true;
+  }
 
-    if (!ibanAccountAddress) {
-        return (<></>);        
-    }
+  const [amount, setAmount] = useState("");
+  const [memo, setMemo] = useState("");
 
-    let qrObject = {
-        name: name,
-        iban: iban,
-    }
+  const [showIbanQR, setShowIbanQR] = useLocalStorage("showIbanQR", true);
+  const [showWarningMessage, setShowWarningMessage] = useLocalStorage("showIbanWarning", true);
 
-    let isValidAmount = false;
-    const amountNumber = parseFloat(parseFloat(amount.replace(/,/g, '.')).toFixed(2));
+  if (!ibanAccountAddress) {
+    return <></>;
+  }
 
-    if (!isNaN(amountNumber) && ((amountNumber >= 0.01) && (amountNumber <= 999999999.99))) {
-        isValidAmount = true;
-        qrObject.amount = amountNumber;
-    }
+  let qrObject = {
+    name: name,
+    iban: iban,
+  };
 
-    if (memo) {
-        qrObject.unstructuredReference = memo;
-    }
+  let isValidAmount = false;
+  const amountNumber = parseFloat(parseFloat(amount.replace(/,/g, ".")).toFixed(2));
 
-    const qr = generateQrCode(qrObject);
+  if (!isNaN(amountNumber) && amountNumber >= 0.01 && amountNumber <= 999999999.99) {
+    isValidAmount = true;
+    qrObject.amount = amountNumber;
+  }
 
-    return (
-        <>
-            {ibanAccountAddressIsDifferent && 
-                <div style={{ display: 'flex', justifyContent: 'space-around', alignItems:"center", paddingBottom:"2em" }}>
-                    {showWarningMessage && 
-                        <div>
-                            <Alert
-                                type={"warning"}
-                                message={
-                                    <div style={{ display: 'flex', flexDirection:"column", justifyContent: 'center', alignItems:"center" }}>
-                                        <div>
-                                            Your <b>IBAN</b> is connected to a different account:
-                                        </div>
-                                        <div style={{ fontWeight:"bold" }}>
-                                            {getShortAddress(ibanAccountAddress)}
-                                        </div>
-                                    </div>
-                                }
-                            />
-                        </div>
-                    }
-                    <div 
-                        style={{ fontSize: "3em", cursor: "pointer", backgroundColor: "" }}
-                        onClick={
-                            () => {
-                                setShowWarningMessage(!showWarningMessage)
-                            } 
-                        }
-                    >
-                        <WarningOutlined style={{ color: showWarningMessage ? "black" : "gray"}}/>
+  if (memo) {
+    qrObject.unstructuredReference = memo;
+  }
+
+  const qr = generateQrCode(qrObject);
+
+  return (
+    <>
+      {ibanAccountAddressIsDifferent && (
+        <div style={{ display: "flex", justifyContent: "space-around", alignItems: "center", paddingBottom: "2em" }}>
+          {showWarningMessage && (
+            <div>
+              <Alert
+                type={"warning"}
+                message={
+                  <div
+                    style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}
+                  >
+                    <div>
+                      Your <b>IBAN</b> is connected to a different account:
                     </div>
-                </div>    
-            }
-
-            <div style={{ display: 'flex', justifyContent: 'space-around', alignItems:"center", backgroundColor: "ghostwhite" }}>
-                <div>
-                    <LogoOnLogo
-                        src1={"EURe.png"}
-                        src2={"polygon-bgfill-icon.svg"}
-                        sizeMultiplier1={3}
-                        sizeMultiplier2={0.5}
-                    />
-                </div>
-
-                <div 
-                    style={{ display: 'flex', flexDirection: 'column', backgroundColor: "", cursor: "pointer" }}
-                    onClick={ () => {
-                        handleCopy(iban);
-                    }}
-                >
-                <div style={{ alignItems: "flex-start" }}>
-                    <div style={{ display: "flex", alignItems: "center", justifyContent:"center", backgroundColor: "", fontWeight: 'bold' }}>
-                        IBAN:
-                    </div>
-                </div>
-                    <div style={{ display: 'flex', alignItems: 'center' }}>
-                        {iban} <CopyOutlined style={{ color: "#000000", fontSize: 18, marginLeft: '0.5em' }} />
-                    </div>
-                </div>
-
-                <div 
-                    style={{ fontSize: "3em", cursor: "pointer", backgroundColor: "" }}
-                    onClick={
-                        () => {
-                            setShowIbanQR(!showIbanQR)
-                        } 
-                    }
-                >
-                    <QrcodeOutlined style={{ color: showIbanQR ? "black" : "gray"}}/>
-                </div>
+                    <div style={{ fontWeight: "bold" }}>{getShortAddress(ibanAccountAddress)}</div>
+                  </div>
+                }
+              />
             </div>
+          )}
+          <div
+            style={{ fontSize: "3em", cursor: "pointer", backgroundColor: "" }}
+            onClick={() => {
+              setShowWarningMessage(!showWarningMessage);
+            }}
+          >
+            <WarningOutlined style={{ color: showWarningMessage ? "black" : "gray" }} />
+          </div>
+        </div>
+      )}
 
-            {showIbanQR && <div style={{ display: 'flex', flexDirection:"column", justifyContent: 'center', alignItems:"center", paddingTop:"2em" }}>
-                <QR
-                    level={"M"}
-                    includeMargin={false}
-                    value={qr}
-                    style={{ height: "auto", maxWidth: "100%", width: "60%" }}
-                />
+      <div
+        style={{ display: "flex", justifyContent: "space-around", alignItems: "center", backgroundColor: "ghostwhite" }}
+      >
+        <div>
+          <LogoOnLogo src1={"EURe.png"} src2={"polygon-bgfill-icon.svg"} sizeMultiplier1={3} sizeMultiplier2={0.5} />
+        </div>
 
-                {name && 
-                    <div>
-                        {name}
-                    </div>
-                }
-                {iban && 
-                    <div>
-                        {iban}
-                    </div>
-                }
-                {isValidAmount && 
-                    <div>
-                        {"€ " + amountNumber}
-                    </div>
-                }
-                {memo && 
-                    <div>
-                        {memo}
-                    </div>
-                }
-
-                <div style={{ display: 'flex', flexDirection:"column", justifyContent: 'center', alignItems:"center", paddingTop:"2em" }}>
-                    <Input
-                        style={{ paddingBottom:"1em" }}
-                        value={amount}
-                        placeholder={"amount in " + "EURe"}
-                        addonAfter={<TokenDisplay token={{name:"EURe", imgSrc:"EURe.png"}}/>}
-                        onChange={async e => {
-                          setAmount(e.target.value);
-                        }}
-                    />
-                    <Input
-                        value={memo}
-                        placeholder={"Thx for the pizza"}
-                        addonAfter={<>Memo</>}
-                        onChange={async e => {
-                          setMemo(e.target.value);
-                        }}
-                    />
-                </div>
+        <div
+          style={{ display: "flex", flexDirection: "column", backgroundColor: "", cursor: "pointer" }}
+          onClick={() => {
+            handleCopy(iban);
+          }}
+        >
+          <div style={{ alignItems: "flex-start" }}>
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                backgroundColor: "",
+                fontWeight: "bold",
+              }}
+            >
+              IBAN:
             </div>
-            }
-        </>
-    );
+          </div>
+          <div style={{ display: "flex", alignItems: "center" }}>
+            {iban} <CopyOutlined style={{ color: "#000000", fontSize: 18, marginLeft: "0.5em" }} />
+          </div>
+        </div>
+
+        <div
+          style={{ fontSize: "3em", cursor: "pointer", backgroundColor: "" }}
+          onClick={() => {
+            setShowIbanQR(!showIbanQR);
+          }}
+        >
+          <QrcodeOutlined style={{ color: showIbanQR ? "black" : "gray" }} />
+        </div>
+      </div>
+
+      {showIbanQR && (
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            paddingTop: "2em",
+          }}
+        >
+          <QR level={"M"} includeMargin={false} value={qr} style={{ height: "auto", maxWidth: "100%", width: "60%" }} />
+
+          {name && <div>{name}</div>}
+          {iban && <div>{iban}</div>}
+          {isValidAmount && <div>{"€ " + amountNumber}</div>}
+          {memo && <div>{memo}</div>}
+
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+              paddingTop: "2em",
+            }}
+          >
+            <Input
+              style={{ paddingBottom: "1em" }}
+              value={amount}
+              placeholder={"amount in " + "EURe"}
+              addonAfter={<TokenDisplay token={{ name: "EURe", imgSrc: "EURe.png" }} />}
+              onChange={async e => {
+                setAmount(e.target.value);
+              }}
+            />
+            <Input
+              value={memo}
+              placeholder={"Thx for the pizza"}
+              addonAfter={<>Memo</>}
+              onChange={async e => {
+                setMemo(e.target.value);
+              }}
+            />
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
