dcff55450aad061b1119bee1ed0e3b686dec2bf8
phipsae <philip.krause88@yahoo.de>
2024-01-21 09:09:30 -0400

    Prettier - ERC20Input.jsx

--- packages/react-app/src/components/ERC20Input.jsx
+++ packages/react-app/src/components/ERC20Input.jsx
@@ -4,13 +4,13 @@ import { Input } from "antd";
 
 import { TokenDisplay } from "./";
 
-export default function ERC20Input({token, amount, setAmount}) {
+export default function ERC20Input({ token, amount, setAmount }) {
   return (
     <div>
       <Input
         value={amount}
         placeholder={"amount in " + token.name}
-        addonAfter={<TokenDisplay token={token}/>}
+        addonAfter={<TokenDisplay token={token} />}
         onChange={async e => {
           setAmount(e.target.value);
         }}
