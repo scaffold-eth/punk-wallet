8dd608c07435132d76b957ec9fe927ac8dc78688
phipsae <philip.krause88@yahoo.de>
2024-01-21 16:45:43 -0400

    updated todos in ERC20Balance.jsx

--- packages/react-app/src/components/ERC20Balance.jsx
+++ packages/react-app/src/components/ERC20Balance.jsx
@@ -21,13 +21,13 @@ export default function ERC20Balance({
 
   const [loading, setLoading] = useState(true);
 
-  // ToDo: Update balance after we hit Send
+  // ToDo: Update balance after we hit Send - solved
 
-  // ToDo: Get rid of the error (when switching networks qickly):
+  // ToDo: Get rid of the error (when switching networks qickly): -- should be solved
   // Can't perform a React state update on an unmounted component. This is a no-op, but it indicates a memory leak in your application. To fix, cancel all subscriptions and asynchronous tasks in a useEffect cleanup function.
   // https://medium.com/doctolib/react-stop-checking-if-your-component-is-mounted-3bb2568a4934
 
-  // ToDo: Switch between token amount and token price, same as how we can switch between ETH and USD
+  // ToDo: Switch between token amount and token price, same as how we can switch between ETH and USD - solved
 
   useEffect(() => {
     async function getPrice() {
