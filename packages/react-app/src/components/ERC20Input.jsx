73e8aa7d83e835cde27c41f6c4cc3456e912bf45
phipsae <philip.krause88@yahoo.de>
2024-01-21 13:19:14 -0400

    added toggle button for switching from ERC20 token to USD in input field, adjusted TokenDisplay.jsx

--- packages/react-app/src/components/ERC20Input.jsx
+++ packages/react-app/src/components/ERC20Input.jsx
@@ -1,18 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { Input } from "antd";
 
 import { TokenDisplay } from "./";
 
 export default function ERC20Input({ token, amount, setAmount }) {
+  const [mode, setMode] = useState(token.name);
+
+  let addonAfter;
+  let prefix;
+
+  const toggleUSDERC20 = () => {
+    if (mode === "USD") {
+      setMode(token.name);
+    } else {
+      setMode("USD");
+    }
+  };
+
+  const switchMode = title => {
+    return (
+      <div
+        onClick={() => {
+          toggleUSDERC20();
+        }}
+      >
+        {title}
+      </div>
+    );
+  };
+
+  // <TokenDisplay token={token} />
+
+  if (mode === "USD") {
+    prefix = "$";
+    addonAfter = switchMode("USD 🔀");
+  } else {
+    prefix = "Ξ";
+    addonAfter = switchMode(token.name + " 🔀");
+  }
+
   return (
     <div>
       <Input
         value={amount}
         placeholder={"amount in " + token.name}
-        addonAfter={<TokenDisplay token={token} />}
+        prefix={prefix}
+        addonAfter={<TokenDisplay token={token} setMode={setMode} mode={mode} toggle />}
         onChange={async e => {
           setAmount(e.target.value);
+          console.log("pressed");
         }}
       />
     </div>
--- packages/react-app/src/components/TokenDisplay.jsx
+++ packages/react-app/src/components/TokenDisplay.jsx
@@ -1,18 +1,48 @@
 import React from "react";
 
-export default function TokenDisplay({ token, divStyle, spanStyle }) {
+export default function TokenDisplay({ token, divStyle, spanStyle, setMode, mode, toggle }) {
+  const toggleUSDERC20 = () => {
+    console.log("pressed");
+    if (mode === "USD") {
+      setMode(token.name);
+    } else {
+      setMode("USD");
+    }
+  };
+
+  const switchMode = () => {
+    return mode === token.name ? (
+      <>
+        <img style={{ height: "1em", width: "1em", marginRight: "0.5em" }} alt="Token symbol" src={token.imgSrc} />
+        <span style={{ ...spanStyle }}>{token.name} 🔀</span>
+      </>
+    ) : (
+      <>💵 USD 🔀</>
+    );
+  };
+
   return (
     <div
       style={{
         display: "flex",
         alignItems: "center",
-        justifyContent: token.name == "" ? "center" : "space-evenly",
+        justifyContent: token.name === "" ? "center" : "space-evenly",
         ...divStyle,
       }}
+      onClick={() => {
+        if (toggle) {
+          toggleUSDERC20();
+        }
+      }}
     >
-      {token?.imgSrc && <img style={{ height: "1em", width: "1em" }} src={token.imgSrc} />}
-
-      <span style={{ ...spanStyle }}>{token.name}</span>
+      {toggle
+        ? token.imgSrc && switchMode()
+        : token.imgSrc && (
+            <>
+              <img style={{ height: "1em", width: "1em" }} alt="Token symbol" src={token.imgSrc} />
+              <span style={{ ...spanStyle }}>{token.name}</span>
+            </>
+          )}
     </div>
   );
 }
