bceab2bb7b48f08a0c3d1535cf9a9fd1fcd4be41
moltam89 <moltam89@gmail.com>
2024-06-05 16:06:23 +0200

    Remove unused code

--- packages/react-app/src/App.jsx
+++ packages/react-app/src/App.jsx
@@ -867,26 +867,6 @@ function App(props) {
         const eip681URL = window.location.href.substring(window.location.href.indexOf("ethereum:"));
 
         parseEIP618(eip681URL, networkSettingsHelper, setTargetNetwork);
-
-        // const eip681Object = parse(eip681URL);
-        // console.log("eip681Object", eip681Object);
-
-        // const chainId = eip681Object.chain_id;
-
-        // handleNetworkByQR(chainId, networkSettingsHelper, setTargetNetwork);
-
-        // const functionName = eip681Object.function_name;
-        // const tokenAddress = eip681Object?.target_address;
-
-        // let toAddress;
-
-        // if (functionName == "transfer" && tokenAddress) {
-        //   localStorage.setItem("switchToTokenAddress", tokenAddress);
-        //   toAddress = eip681Object?.params?.address;
-        // } else {
-        //   localStorage.setItem("switchToEth", true);
-        //   toAddress = eip681Object?.target_address;
-        // }
       }
 
       window.history.pushState({}, "", "/");
